3169b6251cc3a54340e015b99c8b72a3f3488472
Marek Majdak <marek.majdak@sufrago.com>
2020-03-21 12:24:51 +0100

    json string

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -13,7 +13,7 @@ fn mysql_pool_handler(pool: web::Data<MysqlPool>) -> Result<MySqlPooledConnectio
 
 // NOTE: test API without db conneciton;
 // fn users_mock() -> Result<User, Error> {
-//     let _users_json = r#"{"id":"89251ab3-1cdc-4629-9086-ce022cf3549e","first_name":"Marek","last_name":"Majdak","email":"marek.majdak@sufrago.com","name":"sufrago","create_at":"2019-12-17T17:58:07.533406","avatar_id":"1cb15088-afd4-4d00-a7fc-d95eae1abefb","phone_no":"+48505117069","company_name":"Sufrago sp z o.o.","vat_id":"PL 9512468001"}"#;
+//     let _users_json = r#"{"id":"89251ab3-1cdc-4629-9086-ce022cf6669e","first_name":"Marek","last_name":"Majdak","email":"info@sufrago.com","name":"sufrago","create_at":"2019-12-17T17:58:07.533406","avatar_id":"1cb15088-afd4-4d00-a7fc-d95eae1abefb","phone_no":"+48666666666","company_name":"Sufrago sp z o.o.","vat_id":"PL 9512468001"}"#;
 
 //     let user: User = serde_json::from_str(_users_json)?;
 
